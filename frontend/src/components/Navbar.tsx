99c69fdad5c20a820cd7c1bbfcbbdf7b74ceaaa2
Denonpollocks <denon@haloflights.co.uk>
2024-10-26 05:11:30 +0530

    5th commit

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import '../index.css';
 
@@ -35,24 +36,24 @@ const Navbar: React.FC = () => {
 
   const handleMouseEnterRooms = () => {
     clearTimeout(roomsTimeout);
-    setShowRoomsDropdown(true); // Show the dropdown immediately
+    setShowRoomsDropdown(true);
   };
 
   const handleMouseLeaveRooms = () => {
     roomsTimeout = setTimeout(() => {
-      setShowRoomsDropdown(false); // Hide the dropdown after a delay
-    }, 300); // 300ms delay to allow interaction
+      setShowRoomsDropdown(false);
+    }, 300);
   };
 
   const handleMouseEnterExperiences = () => {
     clearTimeout(experiencesTimeout);
-    setShowExperiencesDropdown(true); // Show the dropdown immediately
+    setShowExperiencesDropdown(true);
   };
 
   const handleMouseLeaveExperiences = () => {
     experiencesTimeout = setTimeout(() => {
-      setShowExperiencesDropdown(false); // Hide the dropdown after a delay
-    }, 300); // 300ms delay to allow interaction
+      setShowExperiencesDropdown(false);
+    }, 300);
   };
 
   return (
@@ -63,8 +64,7 @@ const Navbar: React.FC = () => {
         }`}
       >
         <div className="flex items-center justify-between md:px-12 py-3">
-          <a href="/" className="flex items-center">
-            {/* Conditionally show different images based on scroll state */}
+          <Link to="/" className="flex items-center">
             {isScrolled ? (
               <img
                 src="/scrolled-lake-house.png"
@@ -78,92 +78,43 @@ const Navbar: React.FC = () => {
                 alt="Lake House Logo"
               />
             )}
-          </a>
+          </Link>
           <button
             type="button"
             onClick={toggleMenu}
-            className={`focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden lg:hidden hover:text-yellow-100 ${isScrolled ? 'text-darkGreen ' : 'text-lightestGreen '}`}
-            // className="text-lightestGreen md:hidden hover:text-yellow-100 "
+            className={`focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden lg:hidden hover:text-yellow-100 ${
+              isScrolled ? 'text-darkGreen ' : 'text-lightestGreen '
+            }`}
           >
             {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
           </button>
           <div className={`hidden md:flex space-x-8 ${menuOpen ? 'block' : 'hidden'} w-full md:w-auto`}>
             <ul className="flex items-center space-x-6 text-xl alumni-sans-pinstripe-nav font-medium">
-              {/* Desktop menu items */}
-              <li>
-                <a href="/" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen ' : 'text-lightestGreen '}`}>
-                  Home
-                </a>
-              </li>
+              <li><Link to="/" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen ' : 'text-lightestGreen '}`}>Home</Link></li>
               <li className="relative group" onMouseEnter={handleMouseEnterRooms} onMouseLeave={handleMouseLeaveRooms}>
-                <a href="#" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen' : 'text-lightestGreen '}`}>
-                  Suites
-                </a>
+                <Link to="#" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen' : 'text-lightestGreen '}`}>Suites</Link>
                 {showRoomsDropdown && (
                   <div className="absolute left-0 mt-2 w-72 bg-white rounded-md shadow-lg z-20 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                    <a
-                      href="/lake-view-indoor"
-                      className="block px-4 py-2 border-b border-paleGreen text-gray-700 hover:bg-yellow-100"
-                    >
-                      Lake View <span className='text-sm font-light'>(Indoor Shower)</span>
-                    </a>
-                    <a
-                      href="/garden-view-indoor"
-                      className="block px-4 py-2 border-b border-paleGreen text-gray-700 hover:bg-yellow-100"
-                    >
-                      Garden View <span className='text-sm font-light'>(Indoor Shower)</span>
-                    </a>
-                    <a
-                      href="/lake-view-outdoor"
-                      className="block px-4 py-2 border-b border-paleGreen text-gray-700 hover:bg-yellow-100"
-                    >
-                      Lake View <span className='text-sm font-light'>(Outdoor Shower)</span>
-                    </a>
-                    <a href="/garden-view-outdoor" className="block px-4 py-2 text-gray-700 hover:bg-yellow-100">
-                      Garden View <span className='text-sm font-light'>(Outdoor Shower)</span>
-                    </a>
+                    <Link to="/lake-view-indoor" className="block px-4 py-2 border-b border-paleGreen text-gray-700 hover:bg-yellow-100">Lake View (Indoor Shower)</Link>
+                    <Link to="/garden-view-indoor" className="block px-4 py-2 border-b border-paleGreen text-gray-700 hover:bg-yellow-100">Garden View (Indoor Shower)</Link>
+                    <Link to="/lake-view-outdoor" className="block px-4 py-2 border-b border-paleGreen text-gray-700 hover:bg-yellow-100">Lake View (Outdoor Shower)</Link>
+                    <Link to="/garden-view-outdoor" className="block px-4 py-2 text-gray-700 hover:bg-yellow-100">Garden View (Outdoor Shower)</Link>
                   </div>
                 )}
               </li>
-              <li
-                className="relative group"
-                onMouseEnter={handleMouseEnterExperiences}
-                onMouseLeave={handleMouseLeaveExperiences}
-              >
-                <a href="#" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen  ' : 'text-lightestGreen'}`}>
-                  Experiences
-                </a>
+              <li className="relative group" onMouseEnter={handleMouseEnterExperiences} onMouseLeave={handleMouseLeaveExperiences}>
+                <Link to="#" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen  ' : 'text-lightestGreen'}`}>Experiences</Link>
                 {showExperiencesDropdown && (
                   <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg z-20 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                    <a href="/things-to-do" className="block px-4 py-2 border-b border-paleGreen text-gray-700 hover:bg-yellow-100">
-                      Things to Do
-                    </a>
-                    <a href="/places-to-visit" className="block px-4 py-2 text-gray-700 hover:bg-yellow-100">
-                      Places to Visit
-                    </a>
+                    <Link to="/things-to-do" className="block px-4 py-2 border-b border-paleGreen text-gray-700 hover:bg-yellow-100">Things to Do</Link>
+                    <Link to="/places-to-visit" className="block px-4 py-2 text-gray-700 hover:bg-yellow-100">Places to Visit</Link>
                   </div>
                 )}
               </li>
-              <li>
-                <a href="/dining" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen' : 'text-lightestGreen'}`}>
-                  Meals
-                </a>
-              </li>
-              <li>
-                <a href="/events" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen' : 'text-lightestGreen'}`}>
-                  Events
-                </a>
-              </li>
-              <li>
-                <a href="/contact" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen' : 'text-lightestGreen'}`}>
-                  Contact
-                </a>
-              </li>
-              <li>
-                <a href="/about" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen' : 'text-lightestGreen'}`}>
-                  About
-                </a>
-              </li>
+              <li><Link to="/dining" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen' : 'text-lightestGreen'}`}>Meals</Link></li>
+              <li><Link to="/events" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen' : 'text-lightestGreen'}`}>Events</Link></li>
+              <li><Link to="/contact" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen' : 'text-lightestGreen'}`}>Contact</Link></li>
+              <li><Link to="/about" className={`hover:text-yellow-100 ${isScrolled ? 'text-darkGreen' : 'text-lightestGreen'}`}>About</Link></li>
             </ul>
           </div>
         </div>
@@ -182,61 +133,17 @@ const Navbar: React.FC = () => {
           <FaTimes size={24} />
         </button>
         <ul className="flex flex-col space-y-6 mt-20 px-8 text-xl font-medium">
-          <li>
-            <a href="/" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/lake-view-indoor" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>
-              Lake View (Indoor Shower)
-            </a>
-          </li>
-          <li>
-            <a href="/garden-view-indoor" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>
-              Garden View (Indoor Shower)
-            </a>
-          </li>
-          <li>
-            <a href="/lake-view-outdoor" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>
-              Lake View (Outdoor Shower)
-            </a>
-          </li>
-          <li>
-            <a href="/garden-view-outdoor" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>
-              Garden View (Outdoor Shower)
-            </a>
-          </li>
-          <li>
-            <a href="/things-to-do" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>
-            Things to Do
-            </a>
-          </li>
-          <li>
-            <a href="/places-to-visit" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>
-              Places to Visit
-            </a>
-          </li>
-          <li>
-            <a href="/dining" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>
-              Meals
-            </a>
-          </li>
-          <li>
-            <a href="/events" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>
-              Events
-            </a>
-          </li>
-          <li>
-            <a href="/contact" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>
-              Contact
-            </a>
-          </li>
-          <li>
-            <a href="/about" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>
-              About
-            </a>
-          </li>
+          <li><Link to="/" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>Home</Link></li>
+          <li><Link to="/lake-view-indoor" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>Lake View (Indoor Shower)</Link></li>
+          <li><Link to="/garden-view-indoor" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>Garden View (Indoor Shower)</Link></li>
+          <li><Link to="/lake-view-outdoor" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>Lake View (Outdoor Shower)</Link></li>
+          <li><Link to="/garden-view-outdoor" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>Garden View (Outdoor Shower)</Link></li>
+          <li><Link to="/things-to-do" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>Things to Do</Link></li>
+          <li><Link to="/places-to-visit" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>Places to Visit</Link></li>
+          <li><Link to="/dining" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>Meals</Link></li>
+          <li><Link to="/events" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>Events</Link></li>
+          <li><Link to="/contact" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>Contact</Link></li>
+          <li><Link to="/about" className="text-darkGreen hover:text-yellow-100" onClick={toggleMenu}>About</Link></li>
         </ul>
       </div>
     </>
