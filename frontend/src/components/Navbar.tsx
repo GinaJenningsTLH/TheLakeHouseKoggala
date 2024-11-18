3447d8e04870a3e4d622acd22f84cb63be54f892
Denonpollocks <denon@haloflights.co.uk>
2024-11-19 01:38:59 +0530

    commit 42

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FaBars, FaTimes, FaChevronDown } from 'react-icons/fa';
-import '../index.css';
+
 
 const Navbar: React.FC = () => {
   const location = useLocation();
