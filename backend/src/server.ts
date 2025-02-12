7588cfd374264b2a951be0ca23e35967b13bfdf2
Denonpollocks <denon@haloflights.co.uk>
2025-02-13 03:11:37 +0530

    8 com

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,6 +11,8 @@ const app = express();
 const corsOptions = {
   origin: [
     'https://www.thelakehousekoggala.com',
+    'https://thelakehousekoggala.com',
+    'https://thelakehousekoggala-api.onrender.com',
     'http://localhost:5173'
   ],
   methods: ['GET', 'POST', 'OPTIONS'],
