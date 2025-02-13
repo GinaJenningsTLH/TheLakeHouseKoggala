b50a70b11f29f3ee7a1c690e5dd2efca4bc9402c
Denonpollocks <denon@haloflights.co.uk>
2025-02-13 11:54:57 +0530

    8 com

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' 
-    ? 'https://www.thelakehousekoggala.com'
-    : '/',// Adjust if deploying to a subdirectory
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
